13ca4fa3298f08450f2c55dd67e48a420f182746
Varun Kumar <67059001+ign-ite@users.noreply.github.com>
2024-06-24 19:46:22 +0000

    <<<

--- main.py
+++ main.py
@@ -64,3 +64,87 @@ def nextitem():
     global var
     var = var + 1
     f.seek(var)
+ try:
+        c=f.readlines()
+        xyz = c[var]
+        v = list(xyz.split(" "))
+        entry1.delete(0, END)
+        entry2.delete(0, END)
+        entry3.delete(0, END)
+        entry4.delete(0, END)
+        entry5.delete(0, END)
+        entry1.insert(0, str(v[0]))
+        entry2.insert(0, str(v[1]))
+        entry3.insert(0, str(v[2]))
+        entry4.insert(0, str(v[3]))
+        entry5.insert(0, str(v[4]))
+    except:
+        messagebox.showinfo("Title", "SORRY!...NO MORE RECORDS")
+def previousitem():
+        global var
+        var=var-1
+        f.seek(var)
+        try:
+            z = f.readlines()
+            xyz=z[var]
+            v = list(xyz.split(" "))
+            entry1.delete(0, END)
+            entry2.delete(0, END)
+            entry3.delete(0, END)
+            entry4.delete(0, END)
+            entry5.delete(0, END)
+
+            entry1.insert(0, str(v[0]))
+            entry2.insert(0, str(v[1]))
+            entry3.insert(0, str(v[2]))
+            entry4.insert(0, str(v[3]))
+            entry5.insert(0, str(v[4]))
+        except:
+            messagebox.showinfo("Title", "SORRY!...NO MORE RECORDS")
+
+
+def lastitem():
+    global var
+    f4=open("database_proj",'r')
+    x=f4.read().splitlines()
+    last_line= x[-1]
+    num_lines = 0
+    with open("database_proj", 'r') as f8:
+        for line in f8:
+            num_lines += 1
+    var=num_lines-1
+    print(last_line)
+    try:
+        v = list(last_line.split(" "))
+        entry1.delete(0, END)
+        entry2.delete(0, END)
+        entry3.delete(0, END)
+        entry4.delete(0, END)
+        entry5.delete(0, END)
+
+        entry1.insert(0, str(v[0]))
+        entry2.insert(0, str(v[1]))
+        entry3.insert(0, str(v[2]))
+        entry4.insert(0, str(v[3]))
+        entry5.insert(0, str(v[4]))
+    except:
+        messagebox.showinfo("Title", "SORRY!...NO MORE RECORDS")
+
+
+def updateitem():
+
+    e1 = entry1.get()
+    e2 = entry2.get()
+    e3 = entry3.get()
+    e4 = entry4.get()
+    e5 = entry5.get()
+    with open(r"database_proj") as f1, open(r"database_proj1", "w") as working:
+        for line in f1:
+            if str(e1) not in line:
+                working.write(line)
+            else:
+                working.write('{0} {1} {2} {3} {4}'.format(str(e1), e2, e3, str(e4), e5))
+    os.remove(r"database_proj")
+    #brought to you by code-projects.org
+    os.rename(r"database_proj1", r"database_proj")
+
